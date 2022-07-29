60aba24d18e1e247daccd38ab49c0e6e9ce07781
Ali Nemat <84376102+AliNematt@users.noreply.github.com>
2022-07-30 01:46:32 +0430

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,5 @@ submit.addEventListener('click', function () {
         <p class="description">We appreciate you taking the time to give a rating. If you ever need more support, 
         don’t hesitate to get in touch!</p>
         </div> 
-    `
+    `;
 })
-// document.querySelector('input[name="rate"]:checked').value;
-
-{/* <div id="thank">
-<img src="./images/illustration-thank-you.svg" alt="#" class="thanks-img">
-<div class="user-point"><span> You selected 4 out of 5 </span></div>
-<h1 class="title">Thank you!</h1>
-<p class="description">We appreciate you taking the time to give a rating. If you ever need more support, 
-  don’t hesitate to get in touch!</p>
-</div> */}
